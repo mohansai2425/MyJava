d63a514c113392bad489acea2e21f02e1b45e1c6
mohansai2425 <mohan2425edu@gmail.com>
2024-10-06 17:25:34 +0530

    Add Numbers Sum

---
+++ src/Addtwonumbers.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Addtwonumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+
+
+
+    }
+}
